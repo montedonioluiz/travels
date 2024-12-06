5a4ec750f5aa00cfb8e7f7ee57b4c43805d59680
Luiz Montedonio <montedonio.luiz@gmail.com>
2024-12-05 23:31:35 -0300

    init hamburguer menu

--- web/src/components/map.tsx
+++ web/src/components/map.tsx
@@ -1,8 +1,10 @@
 import Zoom from "@/components/zoom";
+import Menu from "@/components/menu";
 
 export default function Map() {
   return (
     <div id="map" className="h-full">
+      <Menu />
       <Zoom />
     </div>
   );
---
+++ web/src/components/menu/index.tsx
@@ -0,0 +1,4 @@
+export default function Menu() {
+  // TODO import icons
+  return <button className="absolute z-50 top-2 right-4">H</button>;
+}
